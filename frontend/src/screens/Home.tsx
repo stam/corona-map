a27cef6e68027e456e67f6acecce672a67352098
Jasper Stam <j.stam@vanberlo.nl>
2020-10-13 19:33:09 +0200

    🀄️ Add tiles

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -6,15 +6,30 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
   display: grid;
-  background: #ccc;
+  background: #f9f9f9;
+  padding: 1rem;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  grid-gap: 1rem;
 `;
 
-const Tile = styled.div``;
+interface TileProps {
+  area: string;
+}
+const Tile = styled.div<TileProps>`
+  background: white;
+  border-radius: 4px;
+  box-shadow: 0 -1px 1px 0 #e5e5e5, 0 1px 1px 0 #e5e5e5, 0 2px 2px 0 #e5e5e5,
+    0 4px 4px 0 #e5e5e5;
+
+  grid-area: ${(props) => props.area};
+`;
 
 export const Home: React.FC = observer(() => {
   return (
     <Container>
-      <p>map</p>
+      <Tile area="1 / 1 / -1 / 7">map</Tile>
+      <Tile area="1 / 7 / -1 / -1">details</Tile>
     </Container>
   );
 });
