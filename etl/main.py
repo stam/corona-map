652d35e8a916d0de2b475cfe6600586a8f748f1b
Jasper Stam <j.stam@vanberlo.nl>
2020-10-17 16:53:29 +0200

    🏓 Add xlsx parsing

--- etl/main.py
+++ etl/main.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xlrd
 
 def convert_measures_to_json(file_path='data_response_graphs_0.csv', output_file_path='measures.json'):
   output = {}
@@ -20,5 +21,40 @@ def convert_measures_to_json(file_path='data_response_graphs_0.csv', output_file
   with open(output_file_path, 'w') as output_file:
     json.dump(output, output_file)
 
+  print(output.keys())
+
+def parse_corona_results(file_path='COVID-19-geographic-disbtribution-worldwide-2020-10-13.xlsx'):
+  output = []
+  data = None
+
+  with xlrd.open_workbook(file_path) as wb:
+    sheet = wb.sheet_by_index(0)
+
+    header = sheet.row_values(0, 1)
+    # for row_index in range(1, sheet.nrows):
+    row = sheet.row_values(1, 1)
+    datum = {a : b for a, b in zip(header, row)}
+
+
+
+
+    # for rownum in range(sheet.nrows):
+    #     wr.writerow(sheet.row_values(rownum))
+
+  # with open(file_path, 'r') as file:
+  #   reader = csv.DictReader(file, dialect='excel')
+  #   data = list(reader)
+
+  # for datum in data:
+  #   print(datum)
+  #   if datum['continent'] == 'Europe':
+  #     country = datum['countriesAndTerritories']
+  #     if country not in output:
+  #       output.append(country)
+
+    print(output)
+
+
 if __name__ == '__main__':
-  convert_measures_to_json()
+  # convert_measures_to_json()
+  parse_corona_results()
