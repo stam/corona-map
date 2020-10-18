48ec73f1a9c9faa897bde0aa72b14ca31e23852f
Jasper Stam <j.stam@vanberlo.nl>
2020-10-18 11:39:43 +0200

    📈  Parse distribution xlsx

--- etl/main.py
+++ etl/main.py
@@ -3,7 +3,8 @@ import json
 import xlrd
 
 # measure_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'United Kingdom']
-# result_countries = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia_and_Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia', 'Denmark', 'Estonia', 'Faroe_Islands', 'Finland', 'France', 'Georgia', 'Germany', 'Gibraltar', 'Greece', 'Guernsey', 'Holy_See', 'Hungary', 'Iceland', 'Ireland', 'Isle_of_Man', 'Italy', 'Jersey', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'North_Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San_Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Ukraine', 'United_Kingdom']
+# result_countries = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia', 'Denmark', 'Estonia', 'Faroe Islands', 'Finland', 'France', 'Georgia', 'Germany', 'Gibraltar', 'Greece', 'Guernsey', 'Holy See', 'Hungary', 'Iceland', 'Ireland', 'Isle of Man', 'Italy', 'Jersey', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'North Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Ukraine', 'United Kingdom']
+measure_countries = []
 
 def convert_measures_to_json(file_path='data_response_graphs_0.csv', output_file_path='measures.json'):
   output = {}
@@ -29,8 +30,8 @@ def convert_measures_to_json(file_path='data_response_graphs_0.csv', output_file
   return output
 
 
-def parse_corona_results(file_path='COVID-19-geographic-disbtribution-worldwide-2020-10-13.xlsx'):
-  output = []
+def parse_corona_results(file_path='COVID-19-geographic-disbtribution-worldwide-2020-10-13.xlsx', output_file_path='distribution.json', country_whitelist=None):
+  output = {}
   data = None
 
   with xlrd.open_workbook(file_path) as wb:
@@ -41,15 +42,20 @@ def parse_corona_results(file_path='COVID-19-geographic-disbtribution-worldwide-
       row = sheet.row_values(row_index, 1)
       datum = {a : b for a, b in zip(header, row)}
 
-      if datum['continentExp'] == 'Europe':
-        country = datum['countriesAndTerritories'].replace('_', ' ')
-        if country not in output:
-          output.append(country)
+      country = datum['countriesAndTerritories'].replace('_', ' ')
+      if country_whitelist is not None:
+        if country not in country_whitelist:
+          continue
+
+      if country not in output:
+        output[country] = []
+
+      output[country].append(datum)
+
+  with open(output_file_path, 'w') as output_file:
+    json.dump(output, output_file)
 
   return output
-
-  # print(output)
-  # result_countries = output
 
 def list_diff(list_a, list_b):
   set_a = set(list_a)
@@ -67,5 +73,6 @@ def list_diff(list_a, list_b):
 
 if __name__ == '__main__':
   measures = convert_measures_to_json()
-  result_countries = parse_corona_results()
-  list_diff(measures, result_countries)
+  countries_with_measures = list(measures.keys())
+  result_countries = parse_corona_results(country_whitelist=countries_with_measures)
+  # list_diff(countries_with_measures, result_countries)
