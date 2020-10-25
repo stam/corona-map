2e019d0d4683f7f99a8af4e8856aef90f4aa1842
Jasper Stam <j.stam@vanberlo.nl>
2020-10-25 14:53:35 +0100

    👓 Refactor etl layer

--- etl/main.py
+++ etl/main.py
@@ -6,6 +6,7 @@ import xlrd
 # result_countries = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia', 'Denmark', 'Estonia', 'Faroe Islands', 'Finland', 'France', 'Georgia', 'Germany', 'Gibraltar', 'Greece', 'Guernsey', 'Holy See', 'Hungary', 'Iceland', 'Ireland', 'Isle of Man', 'Italy', 'Jersey', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'North Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Ukraine', 'United Kingdom']
 measure_countries = []
 
+
 def convert_measures_to_json(file_path='data_response_graphs_0.csv', output_file_path='../frontend/src/store/measures.json'):
   output = {}
   measures = None
@@ -21,7 +22,6 @@ def convert_measures_to_json(file_path='data_response_graphs_0.csv', output_file
 
     output[country].append(measure)
 
-
   with open(output_file_path, 'w') as output_file:
     json.dump(output, output_file)
 
@@ -29,60 +29,134 @@ def convert_measures_to_json(file_path='data_response_graphs_0.csv', output_file
   # measure_countries = list(output.keys())
   return output
 
-# 1.0 to 01
-# 30.0 to 30
-def float_to_date(date):
-  return str(int(date)).zfill(2)
 
-def parse_datum(datum):
-  output = datum
+class DataStore():
+  # indexed by date -> country
+  date_summary = {}
+  # indexed by country -> date
+  country_summary = {}
 
-  if 'year' not in datum:
-    print(datum)
+  date_summary_path = '../frontend/public/world.json'
+  country_summary_path = '../frontend/public/countries.json'
 
-  year = float_to_date(datum['year'])
-  month = float_to_date(datum['month'])
-  day = float_to_date(datum['day'])
+  def _set(self, target_dict, keys, val):
+    '''
+    a = {}
+    store.set(a, ['b', 'c', 'd'], 'henk)
+    => a = {'b': {'c': {'d': 'henk'}}}
+    '''
+    last_key = keys[-1]
+    d = target_dict
 
-  date = f'{year}-{month}-{day}'
-  del output['year']
-  del output['month']
-  del output['day']
-  output['date'] = date
+    for key in keys[:-1]:
+      if key not in d:
+        d[key] = {}
+      d = d[key]
 
-  output['cum_14day_100k'] = datum['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']
-  del output['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']
+    d[last_key] = val
 
-  return output
+  def parse_case_row(self, case_row):
+    date = case_row['date']
+    country = case_row['country']
 
-def parse_corona_results(file_path='COVID-19-geographic-disbtribution-worldwide-2020-10-20.xlsx', output_file_path='../frontend/src/store/distribution.json', country_whitelist=None):
+    if case_row['continent'] != 'Europe':
+      return
+
+    summary = {
+        'biweeklyTotalPer100k': case_row['biweeklyTotalPer100k'],
+        'cases': case_row['cases'],
+        'deaths': case_row['deaths']
+    }
+
+    self._set(self.date_summary, [date, country], summary)
+    self._set(self.country_summary, [country, date], summary)
+
+  def write_to_file(self):
+    with open(self.date_summary_path, 'w') as output_file:
+      json.dump(self.date_summary, output_file)
+
+    with open(self.country_summary_path, 'w') as output_file:
+      json.dump(self.country_summary, output_file)
+
+    return
+
+
+store = DataStore()
+
+
+class DistributionParser():
+  file_path = 'COVID-19-geographic-disbtribution-worldwide-2020-10-20.xlsx'
+
+  # 1.0 to 01
+  # 30.0 to 30
+  def float_to_date(self, date):
+    return str(int(date)).zfill(2)
+
+  def parse_row(self, input_row):
+    output = {}
+
+    if 'year' not in input_row:
+      print(input_row)
+
+    year = self.float_to_date(input_row['year'])
+    month = self.float_to_date(input_row['month'])
+    day = self.float_to_date(input_row['day'])
+
+    date = f'{year}-{month}-{day}'
+    output['date'] = date
+
+    output['biweeklyTotalPer100k'] = input_row['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']
+    output['country'] = input_row['countriesAndTerritories'].replace('_', ' ')
+    output['cases'] = input_row['cases']
+    output['deaths'] = input_row['deaths']
+    output['continent'] = input_row['continentExp']
+
+    return output
+
+  def parse(self):
+    with xlrd.open_workbook(self.file_path) as wb:
+      sheet = wb.sheet_by_index(0)
+
+      header = sheet.row_values(0, 1)
+      for row_index in range(1, sheet.nrows):
+        row = sheet.row_values(row_index, 1)
+        row_dict = {a: b for a, b in zip(header, row)}
+
+        parsed_row = self.parse_row(row_dict)
+
+        store.parse_case_row(parsed_row)
+
+        # country = row_dict['countriesAndTerritories'].replace('_', ' ')
+        # if country_whitelist is not None:
+        #   if country not in country_whitelist:
+        #     continue
+        # else:
+        #   if row_dict['continentExp'] != 'Europe':
+        #     continue
+
+        # output[country].append(d)
+
+
+distributionParser = DistributionParser()
+
+
+def parse_hospital_data(file_path='hospital_data.csv'):
   output = {}
-  data = None
+  hosp = None
+  with open(file_path, 'r') as file:
+    reader = csv.DictReader(file)
+    hosp = list(reader)
 
-  with xlrd.open_workbook(file_path) as wb:
-    sheet = wb.sheet_by_index(0)
+  print(hosp[0])
 
-    header = sheet.row_values(0, 1)
-    for row_index in range(1, sheet.nrows):
-      row = sheet.row_values(row_index, 1)
-      datum = {a : b for a, b in zip(header, row)}
+  # for measure in hosp:
+  #   country = measure['Country']
 
-      country = datum['countriesAndTerritories'].replace('_', ' ')
-      if country_whitelist is not None:
-        if country not in country_whitelist:
-          continue
+  #   if country not in output:
+  #     output[country] = []
 
-      if country not in output:
-        output[country] = []
+  #   output[country].append(measure)
 
-      d = parse_datum(datum)
-
-      output[country].append(d)
-
-  with open(output_file_path, 'w') as output_file:
-    json.dump(output, output_file)
-
-  return output
 
 def list_diff(list_a, list_b):
   set_a = set(list_a)
@@ -99,7 +173,10 @@ def list_diff(list_a, list_b):
 
 
 if __name__ == '__main__':
-  measures = convert_measures_to_json()
-  countries_with_measures = list(measures.keys())
-  results = parse_corona_results(country_whitelist=countries_with_measures)
+  distributionParser.parse()
+  store.write_to_file()
+  # measures = convert_measures_to_json()
+  # countries_with_measures = list(measures.keys())
+  # results = parse_cases()
+  # hospital = parse_hospital_data()
   # list_diff(countries_with_measures, result_countries)
